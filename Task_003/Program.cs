b8aa81ca10b6337eaef4af225957d465066a1146
KorolevAV <antoshka.992@mail.ru>
2021-12-20 21:53:33 +0300

    add task_003

---
+++ Task_003/Program.cs
@@ -0,0 +1,30 @@
+﻿int a = 1, b = 2, c = 3, d = 4, e = 5, f = 6, g = 7;
+int x = 5;
+if (a == x)
+{
+    Console.WriteLine("Понедельник");
+}
+if (b == x)
+{
+    Console.WriteLine("Вторник");
+}
+if (c == x)
+{
+    Console.WriteLine("Среда");
+}
+if (d == x)
+{
+    Console.WriteLine("Четверг");
+    }
+if (e == x)
+{
+    Console.WriteLine("Пятница");
+}
+if (f == x)
+{
+    Console.WriteLine("Суббота");
+}
+if (g == x)
+{
+    Console.WriteLine("Воскресенье");
+}
