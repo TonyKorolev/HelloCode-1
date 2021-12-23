fc71f7d9d5a64d4ff6b38e9a3c645e3edf83f0c3
KorolevAV <antoshka.992@mail.ru>
2021-12-23 21:48:30 +0300

    add task_012

---
+++ Task_012/Program.cs
@@ -0,0 +1,7 @@
+﻿int a = new Random().Next(100, 999);
+Console.WriteLine(a);
+int b = a/100;
+int c = a%10;
+Console.Write("После удаления второй цифры получаем ");
+Console.Write(b);
+Console.Write(c);
