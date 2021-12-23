c2e5f873cb920a3adf0a55bf0fa556d562793778
KorolevAV <antoshka.992@mail.ru>
2021-12-23 21:40:08 +0300

    add task_011

---
+++ Task_011/Program.cs
@@ -0,0 +1,24 @@
+﻿int a = new Random().Next(10, 99);
+Console.WriteLine(a);
+int b = a/10;
+int c = a%10;
+int max = 0;
+if (b==c)
+{
+    Console.WriteLine("Обе цифры равны");
+}
+else
+{
+    if (b>c) 
+    {
+        max = b;
+    }
+    else
+    {
+        max = c;
+    }
+    Console.Write("Наибольшая цифра числа ");
+    Console.Write(a);
+    Console.Write(" равна ");
+    Console.Write(max);
+}
