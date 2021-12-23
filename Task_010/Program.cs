7bb5baa267b60bb798cfe6a7f1495e69956e4bb9
KorolevAV <antoshka.992@mail.ru>
2021-12-23 21:23:36 +0300

    add task_010

---
+++ Task_010/Program.cs
@@ -0,0 +1,8 @@
+﻿int a = new Random().Next(100, 999);
+Console.WriteLine(a);
+int b = a/10;
+int c = b%10;
+Console.Write("Вторая цифра трехзначного числа ");
+Console.Write(a);
+Console.Write(" есть ");
+Console.Write(c);
