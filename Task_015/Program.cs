a7db77299283bc2ba15fd7c1090e4ad7b326ee55
KorolevAV <antoshka.992@mail.ru>
2021-12-30 20:47:32 +0300

    add task_015v2

--- Task_015/Program.cs
+++ Task_015/Program.cs
@@ -1,11 +1,8 @@
 ﻿int A = new Random().Next(100, 10000);
 Console.WriteLine(A);
-if (A%7==0)
+if (A%7==0 && A%23==0)
 {
-    if (A%23==0)
-    {
-        Console.WriteLine($"Число {A} кратно 7 и 23");
-    }
+    Console.WriteLine($"Число {A} кратно 7 и 23");
 }
 else
 {
