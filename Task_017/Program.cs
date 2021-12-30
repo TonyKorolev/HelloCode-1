a163e327c300eb1463d6f88b0980dbcda39dc3c7
KorolevAV <antoshka.992@mail.ru>
2021-12-30 21:38:55 +0300

    add task_018

---
+++ Task_017/Program.cs
@@ -0,0 +1,26 @@
+﻿string Find (int A, int B)
+{
+    string str = String.Empty;
+    if (A*A==B)
+    {
+        str = ($"Число {B} является квадратом числа {A}");
+    }
+    else
+    {
+        str = ("Одно число не является квадратом другого");
+    }
+    if (B*B==A)
+    {
+        str = ($"Число {A} является квадратом числа {B}");
+    }
+    else
+    {
+        str = ("Одно число не является квадратом другого");
+    }
+    return str;
+}
+
+int A = 5;
+int B = 25;
+string res = Find(A,B);
+Console.WriteLine(res);
---
+++ Task_018/Program.cs
@@ -0,0 +1,13 @@
+﻿// Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+bool check(bool x, bool y)
+{
+    if (!(x || y) == (!x && !y))
+        return true;
+    else
+        return false;
+}
+
+if (check(false, false) && check(true, true) && check(true, false) && check(false, true))
+    Console.WriteLine("Верно");
+else
+    Console.WriteLine("Неверно");
