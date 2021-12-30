9b43d1e0a0b76739ef68d85727bbb451647e2287
KorolevAV <antoshka.992@mail.ru>
2021-12-30 20:20:51 +0300

    add task_015

---
+++ Task_014/Program.cs
@@ -0,0 +1,14 @@
+﻿int FindThirdDigit(int N);
+{
+    string str = N.ToString();
+    int[] b = new int[str.Length];
+    for( int i=0; i< str.Length; i++)
+    {
+        b[i] = int.Parse(str[i].ToString());
+    }
+    Console.WriteLine("Третья цифра равна ");
+    return b[2];
+}
+int A = new Random().Next(1, 10000);
+Console.WriteLine(A);
+int s = FindThirdDigit(A);
---
+++ Task_015/Program.cs
@@ -0,0 +1,13 @@
+﻿int A = new Random().Next(100, 10000);
+Console.WriteLine(A);
+if (A%7==0)
+{
+    if (A%23==0)
+    {
+        Console.WriteLine($"Число {A} кратно 7 и 23");
+    }
+}
+else
+{
+    Console.WriteLine($"Число {A} не кратно 7 и 23");
+}
