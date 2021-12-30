8cdf2fba7ffcc8baaed992a587bce227545f4146
KorolevAV <antoshka.992@mail.ru>
2021-12-30 20:45:18 +0300

    add task_016

---
+++ Task_016/Program.cs
@@ -0,0 +1,10 @@
+﻿int Day = new Random().Next(1, 8);
+Console.WriteLine(Day);
+if (Day == 6 || Day == 7)
+{
+    Console.WriteLine("День недели является выходным");
+}
+else
+{
+    Console.WriteLine("День недели не является выходным");
+}
