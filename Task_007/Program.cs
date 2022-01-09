0429937bf11cbfcd3f6035e8619bd3f1fa876a0b
KorolevAV <antoshka.992@mail.ru>
2022-01-09 14:41:25 +0300

    add comment, update task 007

--- Task_007/Program.cs
+++ Task_007/Program.cs
@@ -1,9 +1,16 @@
-﻿int N = 5;
-int b = 2*N+1;
-int index = 0;
-while (index < b)
+﻿//  7. Показать числа от -N до N
+string numbers (int N)
 {
-    Console.Write(-N+index);
-    Console.Write(" ");
-    index++;
+    int count = 2*N+1;
+    int index = 0;
+    string row = String.Empty;
+    while (index < count)
+        {
+        row = row + $" {-N+index}";
+        index++;
+    }
+    return row;
 }
+int N = new Random().Next(5, 21);
+string answer = numbers(N);
+Console.WriteLine(answer);
