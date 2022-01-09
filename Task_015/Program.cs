34cbe1df635eb30dabfb77ea9b50aacf41a1a6c8
KorolevAV <antoshka.992@mail.ru>
2022-01-09 17:52:07 +0300

    add comment, update task 015

--- Task_015/Program.cs
+++ Task_015/Program.cs
@@ -1,10 +1,20 @@
-﻿int A = new Random().Next(100, 10000);
+﻿// 15. Дано число. Проверить кратно ли оно 7 и 23
+
+int A = new Random().Next(7, 200);
 Console.WriteLine(A);
 if (A%7==0 && A%23==0)
 {
     Console.WriteLine($"Число {A} кратно 7 и 23");
 }
+else if (A%7==0 && !(A%23==0))
+{
+    Console.WriteLine($"Число {A} кратно 7 и не кратно 23");
+}
+else if (!(A%7==0) && A%23==0)
+{
+    Console.WriteLine($"Число {A} не кратно 7 и кратно 23");
+}
 else
 {
-    Console.WriteLine($"Число {A} не кратно 7 и 23");
+    Console.WriteLine($"Число {A} не кратно ни 7, ни 23");
 }
