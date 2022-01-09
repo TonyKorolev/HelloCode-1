a9ae9cd756953e548231faa57acdec63c80133d0
KorolevAV <antoshka.992@mail.ru>
2022-01-09 15:44:50 +0300

    add comment, update task 010

--- Task_010/Program.cs
+++ Task_010/Program.cs
@@ -1,8 +1,7 @@
-﻿int a = new Random().Next(100, 999);
+﻿// 10. Показать вторую цифру трёхзначного числа
+
+int a = new Random().Next(100, 999);
 Console.WriteLine(a);
 int b = a/10;
 int c = b%10;
-Console.Write("Вторая цифра трехзначного числа ");
-Console.Write(a);
-Console.Write(" есть ");
-Console.Write(c);
+Console.Write($"Вторая цифра трехзначного числа {a} есть {c}");
