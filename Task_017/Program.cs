2ef0a0fd8e950c3eecbf60b80b7ffc83bd546c9a
KorolevAV <antoshka.992@mail.ru>
2022-01-09 17:57:07 +0300

    add comment, update task 017

--- Task_017/Program.cs
+++ Task_017/Program.cs
@@ -1,15 +1,13 @@
-﻿string Find (int A, int B)
+﻿// 17. По двум заданным числам проверять является ли одно квадратом другого
+
+string Find (int A, int B)
 {
     string str = String.Empty;
     if (A*A==B)
     {
         str = ($"Число {B} является квадратом числа {A}");
     }
-    else
-    {
-        str = ("Одно число не является квадратом другого");
-    }
-    if (B*B==A)
+    else if (B*B==A)
     {
         str = ($"Число {A} является квадратом числа {B}");
     }
@@ -20,7 +18,7 @@
     return str;
 }
 
-int A = 5;
-int B = 25;
+int A = 36;
+int B = 6;
 string res = Find(A,B);
 Console.WriteLine(res);
