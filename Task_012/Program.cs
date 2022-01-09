0c0b71fbcebbcabd6f397351bd842b3d64c8fb45
KorolevAV <antoshka.992@mail.ru>
2022-01-09 15:50:47 +0300

    add comment, update task 012

--- Task_012/Program.cs
+++ Task_012/Program.cs
@@ -1,7 +1,8 @@
-﻿int a = new Random().Next(100, 999);
+﻿// 12. Удалить вторую цифру трёхзначного числа
+
+int a = new Random().Next(100, 999);
 Console.WriteLine(a);
 int b = a/100;
 int c = a%10;
-Console.Write("После удаления второй цифры получаем ");
-Console.Write(b);
-Console.Write(c);
+int answer = b*10+c;
+Console.Write($"После удаления второй цифры получаем {answer}");
