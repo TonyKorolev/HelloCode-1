af6f679ec07ed88c21d0a35182a959c3cf86f9f4
KorolevAV <antoshka.992@mail.ru>
2022-01-13 21:26:44 +0300

    add task 026

---
+++ Task_026/Program.cs
@@ -0,0 +1,18 @@
+﻿// 26. Возведите число А в натуральную степень B используя цикл
+int f(int A, int B)
+{
+    int count = 1;
+    int degree = 1;
+    while (count <= B)
+    {
+        degree = degree*A;
+        count++;
+    }
+    return degree;
+}
+Console.WriteLine("Введите число");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("В какую степень возвести?");
+int B = Convert.ToInt32(Console.ReadLine());
+int N = f(A, B);
+Console.WriteLine(N);
