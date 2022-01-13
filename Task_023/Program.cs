9cc6537b61142e27ddb3ed8cf6de40fcd99a0b15
KorolevAV <antoshka.992@mail.ru>
2022-01-13 20:48:58 +0300

    add task 023

---
+++ Task_023/Program.cs
@@ -0,0 +1,8 @@
+﻿// 23. Показать таблицу квадратов чисел от 1 до N 
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i < N+1; i++)
+{
+    int square = i*i;
+    Console.WriteLine($"{i} * {i} = {square}");
+}
