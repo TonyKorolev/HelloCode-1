0c9de196c497b7b39843b4b392179998a09bd137
KorolevAV <antoshka.992@mail.ru>
2022-01-13 21:52:25 +0300

    add task 027

---
+++ Task_027/Program.cs
@@ -0,0 +1,16 @@
+﻿// 27. Определить количество цифр в числе
+int Numbers (int A)
+{
+    int count = 0;
+    while (A != 0)
+    {
+        A = A/10;
+        count++;
+    }
+    return count;
+}
+
+Console.WriteLine("Введите число");
+int A = Convert.ToInt32(Console.ReadLine());
+int N = Numbers (A);
+Console.WriteLine($"В числе {A} {N} цифр");
