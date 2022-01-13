536337b8ef7859df198906112a453a955d46363c
KorolevAV <antoshka.992@mail.ru>
2022-01-13 22:09:06 +0300

    add task 028

---
+++ Task_028/Program.cs
@@ -0,0 +1,17 @@
+﻿// 28. Подсчитать сумму цифр в числе
+int SumNumbers (int A)
+{
+    int sum = 0;
+    while (A > 0)
+    {
+        int B = A % 10;
+        A = A/10;
+        sum = sum + B;
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите число");
+int A = Convert.ToInt32(Console.ReadLine());
+int N = SumNumbers (A);
+Console.WriteLine($"В числе {A} сумма цифр равна {N}");
