618efeb671467fd031d0251abb5fd82ce6c8ee8b
KorolevAV <antoshka.992@mail.ru>
2022-01-13 21:01:26 +0300

    add task 025

---
+++ Task_025/Program.cs
@@ -0,0 +1,9 @@
+﻿// 25. Найти сумму чисел от 1 до А
+Console.WriteLine("Введите число");
+int A = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+for (int i = 1; i <= A; i++)
+{
+    sum = sum + i;
+}
+Console.WriteLine($"Сумма чисел от 1 до {A} равна {sum}");
