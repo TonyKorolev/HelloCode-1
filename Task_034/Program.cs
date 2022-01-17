bb3e56b64755a00cd7bf50a5213a4c9a52e58baa
KorolevAV <antoshka.992@mail.ru>
2022-01-17 22:03:47 +0300

    update task 034

---
+++ Task_034/Program.cs
@@ -0,0 +1,34 @@
+﻿// 34. Написать программу замену элементов массива на противоположные
+// Создание массива
+void CreatArray (int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-50, 50);
+    }
+}
+
+// Вывод массива
+void PrintArray (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+// Замена на противоположные элементы массивы
+void ChangeArray (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]*(-1)} ");
+    }
+    Console.WriteLine();
+}
+
+int [] array = new int [12];
+CreatArray (array);
+PrintArray (array);
+ChangeArray (array);
