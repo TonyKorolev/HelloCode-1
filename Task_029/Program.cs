ec65c10a4455998406a9def644f92cad99c96eb9
KorolevAV <antoshka.992@mail.ru>
2022-01-17 18:48:58 +0300

    add task 029

---
+++ Task_029/Program.cs
@@ -0,0 +1,14 @@
+﻿// 29. Написать программу вычисления произведения чисел от 1 до N
+int Add (int N)
+{
+    int adding = 1;
+    for (int i = 1; i <= N; i++)
+    {
+        adding = adding * i;
+    }
+    return adding;
+}
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+int answer = Add(N);
+Console.WriteLine($"Произведение чисел от 1 до {N} равна {answer}");
