3cef74b33b5d7798c3f22634e5cdb9c82e35857d
KorolevAV <antoshka.992@mail.ru>
2022-01-20 21:51:52 +0300

    add task 041

---
+++ Task_041/Program.cs
@@ -0,0 +1,21 @@
+﻿// 41. Выяснить являются ли три числа сторонами треугольника
+
+void triangle(int A, int B, int C)
+{
+    if (((A + B) > C) && ((A + C) > B) && ((B + C) > A))
+    {
+        Console.WriteLine("Три числа являются сторонами треугольника");
+    }
+    else
+    {
+        Console.WriteLine("Три числа не являются сторонами треугольника");
+    }
+}
+
+Console.WriteLine("Введите длину первой стороны треугольника");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите длину второй стороны треугольника");
+int B = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите длину первой стороны треугольника");
+int C = Convert.ToInt32(Console.ReadLine());
+triangle(A, B, C);
