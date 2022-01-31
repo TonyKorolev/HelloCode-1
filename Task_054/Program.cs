845adb96c0adcc6ebccbb72e172ec7a114095116
KorolevAV <antoshka.992@mail.ru>
2022-01-31 18:38:04 +0300

    add task 054

---
+++ Task_054/Program.cs
@@ -0,0 +1,49 @@
+﻿// 54. В матрице чисел найти сумму элементов главной диагонали
+
+// Создание двумерного массива
+void CreatMatrix (int [,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(-10, 10);
+        }
+    }
+}
+
+// Вывод двумерного массива
+void PrintMatrix (int [,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+// Сумма элементов главной диагонали
+int SumDiagMatrix (int [,] matrix)
+{
+    int SumDiag = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i == j) SumDiag = SumDiag + matrix[i,j];
+        }
+    }
+    return SumDiag;
+}
+
+Console.WriteLine("Введите число строк и столбцов квадратного двумерного массива");
+int n = Convert.ToInt32(Console.ReadLine());
+int [,] matrix = new int [n, n];
+CreatMatrix (matrix);
+PrintMatrix (matrix);
+int SumDiag = SumDiagMatrix (matrix);
+Console.WriteLine($"Сумма элементов главной диагонали равна {SumDiag}");
