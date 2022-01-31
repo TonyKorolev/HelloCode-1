27c74f41d9b3487900f4b99713794851f2231515
KorolevAV <antoshka.992@mail.ru>
2022-01-31 22:12:43 +0300

    add task 068

---
+++ Task_068/Program.cs
@@ -0,0 +1,15 @@
+﻿// 68. Показать натуральные числа от M до N, N и M заданы
+void PrintArray(int n, int m)
+{
+    Console.Write($"{m} ");
+    if (m != n)
+        PrintArray(n, m + 1);
+}
+
+Console.WriteLine("Введите первое число: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int N = Convert.ToInt32(Console.ReadLine());
+PrintArray(N, M);
+
+
